53a70a1ccef0f3be3c761e5089bd94135a53d8f2
PaweliOS <mirrorpawel.pabisiak@gmail.com>
2022-11-15 00:26:10 +0100

    Added CoinGecko img to crypto__coins--btc, changed classic function (NFN) (get) to async function getPrice.

--- js/script.js
+++ js/script.js
@@ -4,16 +4,24 @@ const btcPrice = document.querySelector('.crypto__coins--btc-price')
 const URL = 'https://api.coingecko.com/api/v3/exchange_rates'
 
 
-
+// =============== obsługa =================
 // const getPrice = () => {
 //     fetch(URL)
 //     .then(res => res.json())
 //         .then(data => console.log(data))
-//         .catch(err => console.log(err)) 
+//         .catch(err => console.log(err))
 // }
-const getPrice = () => {
-    axios.get(URL).then(res => console.log(res.data.rates.usd.value))
-    axios.get(URL).then(res => (btcPrice.textContent = res.data.rates.usd.value + ' USD'))
+
+//  ============ klasycznie =================
+// const getPrice = () => {
+//     axios.get(URL).then(res => console.log(res.data.rates.usd.value))
+//     axios.get(URL).then(res => (btcPrice.textContent = res.data.rates.usd.value + ' USD'))
+// }
+
+//========== funkcja asynchroniczna ==============
+async function getPrice() {
+    const resp = await axios.get(URL)
+    btcPrice.textContent = resp.data.rates.usd.value + ' USD'
 }
 
 checkAllBtn.addEventListener('click', getPrice)
