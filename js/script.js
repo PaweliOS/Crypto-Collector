da07cc375c23c3eaaea7d8f2e0395313348cef06
PaweliOS <mirrorpawel.pabisiak@gmail.com>
2022-11-15 02:34:45 +0100

    Added separated row for ExBitronData crypto__data--exBitron and two crypto coins QOGE and PLSR, added networking for them (ExBitron API) - script.js.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const checkAllBtn = document.querySelector('.buttons__btn-checkAll')
 const btcPrice = document.querySelector('.crypto__coins--btc-price')
 const ethPrice = document.querySelector('.crypto__coins--eth-price')
+const qogePrice = document.querySelector('.crypto__coins--qoge-price')
+const plsrPrice = document.querySelector('.crypto__coins--plsr-price')
 
 // const URL = 'https://api.coingecko.com/api/v3/exchange_rates'
 
@@ -9,10 +11,12 @@ const BTC = 'bitcoin'
 const USD = 'usd'
 const ETH = 'ethereum'
 const EXBITRON_qogeusdt = 'qogeusdt'
+const EXBITRON_plsrusdt = 'plsrusdt'
 const URL_BTC_USD = 'https://api.coingecko.com/api/v3/simple/price?ids='+BTC+'&vs_currencies='+USD
 const URL_ETH_USD = 'https://api.coingecko.com/api/v3/simple/price?ids=' + ETH + '&vs_currencies=' + USD
 
 const URL_EXBITRON_BTC = 'https://www.exbitron.com/api/v2/peatio/public/markets/'+EXBITRON_qogeusdt+'/order-book'
+const URL_EXBITRON_PLSR = 'https://www.exbitron.com/api/v2/peatio/public/markets/'+EXBITRON_plsrusdt+'/order-book'
 
 // === poniżej dla jednego coina (ethereum) pełna informacja =====
 // const URL = 'https://api.coingecko.com/api/v3/coins/ethereum'
@@ -45,8 +49,13 @@ async function getPrice() {
     const respBtc = await axios.get(URL_BTC_USD)
     const respEth = await axios.get(URL_ETH_USD)
     const respExbitronQoge = await axios.get(URL_EXBITRON_BTC)
+    const respExbitronPlsr = await axios.get(URL_EXBITRON_PLSR)
     
-    console.log(respExbitronQoge.data)
+    // console.log(respExbitronQoge.data.bids[0].price)
+    // console.log(respExbitronQoge.data.bids[0].price)
+    
+    qogePrice.textContent = respExbitronQoge.data.bids[0].price + ' USD'
+    plsrPrice.textContent = respExbitronPlsr.data.bids[0].price + ' USD'
     btcPrice.textContent = respBtc.data.bitcoin.usd + ' USD'
     ethPrice.textContent = respEth.data.ethereum.usd + ' USD'
     
